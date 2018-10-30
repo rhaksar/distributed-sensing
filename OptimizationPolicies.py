e79791d2074b273ff7fe044ca0f63a729f2c5a25
Ravi Haksar <rhaksar@stanford.edu>
2018-10-30 17:56:56 +0100

    Created functions to plot images in global frame, map (x,y) to (row, col), and solve a convex program to generate a trajectory plan. Also converted tasks to global frame and set up a simple single agent example.

--- OptimizationPolicies.py
+++ OptimizationPolicies.py
@@ -5,6 +5,7 @@ import cvxpy
 import matplotlib.patches as patches
 import matplotlib.pyplot as pyplot
 import numpy as np
+import operator
 from scipy.stats import multivariate_normal
 from scipy import ndimage
 
@@ -14,8 +15,7 @@ from mpl_toolkits.mplot3d import Axes3D
 '''
 Given a forest state or state slice, create a visualization
 '''
-def PlotForest(state, grid_size):
-
+def PlotForest(state):
     fig = pyplot.figure()
     ax = fig.add_subplot(111, aspect='equal')
     # pyplot.xlim([0, grid_size/2])
@@ -24,10 +24,10 @@ def PlotForest(state, grid_size):
     #                   labelbottom=False, labelleft=False,
     #                   bottom=False, left=False)
 
-    for r in range(grid_size):
-        for c in range(grid_size):
+    for r in range(state.shape[0]):
+        for c in range(state.shape[1]):
             x = col_to_x(c)
-            y = row_to_y(grid_size, r)
+            y = row_to_y(state.shape[1], r)
 
             rec = patches.Rectangle((x, y), 0.5, 0.5, alpha=0.6)
             if state[r, c] == 0:
@@ -41,29 +41,55 @@ def PlotForest(state, grid_size):
 
     return ax
 
+def PlotForestImage(image, position):
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111, aspect='equal')
+
+    cx, cy = col_to_x(image.shape[1]-1)/2 + 0.25, row_to_y(image.shape[0], 0)/2 + 0.25
+
+    for r in range(image.shape[0]):
+        for c in range(image.shape[1]):
+            x = col_to_x(c)
+            y = row_to_y(image.shape[1], r)
+
+            x += position[0] - cx
+            y += position[1] - cy
+
+            rec = patches.Rectangle((x, y), 0.5, 0.5, alpha=0.6)
+            if image[r, c] == 0:
+                rec.set_color('g')
+            elif image[r, c] == 1:
+                rec.set_color('r')
+            elif image[r, c] == 2:
+                rec.set_color('k')
+
+            ax.add_patch(rec)
+
+    return ax
+
 
 def col_to_x(col):
     return 0.5*col
 
 
 def row_to_y(grid_size, row):
-    return 0.5*(grid_size - row - 1)
+    return 0.5*(grid_size-row-1)
 
 
 def rc_to_xy(grid_size, rc):
     return col_to_x(rc[1]), row_to_y(grid_size, rc[0])
 
 
-# def x_to_col(x):
-#     return int(2*x)
-#
-#
-# def y_to_row(grid_size, y):
-#     return int(grid_size - 1 - 2*y)
-#
-#
-# def xy_to_rc(grid_size, xy)
-#     return y_to_row(grid_size, xy[1]), x_to_col(xy[0])
+def x_to_col(x):
+    return np.around(2*x, decimals=1).astype(np.int8)
+
+
+def y_to_row(grid_size, y):
+    return np.around(grid_size-1-2*y, decimals=1).astype(np.int8)
+
+
+def xy_to_rc(grid_size, xy):
+    return y_to_row(grid_size, xy[1]), x_to_col(xy[0])
 
 
 '''
@@ -90,10 +116,10 @@ def CreateImage(state, position, dim):
 '''
 Given an image, create an ordered lists of tasks (locations) and their weights
 '''
-def CreateTasks(image):
+def CreateTasks(image, location, memory):
     cx, cy = col_to_x(image.shape[1]-1)/2 + 0.25, row_to_y(image.shape[0], 0)/2 + 0.25
     tasks = []
-    neighbors = [(-1,0), (1,0), (0,-1), (0, 1)]
+    neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     expand_image = np.pad(image, ((1, 1), (1, 1)), 'constant', constant_values=((-1, -1), (-1, -1)))
 
@@ -101,30 +127,71 @@ def CreateTasks(image):
     if len(fire[0]) >= 1:
         for _, (r, c) in enumerate(zip(fire[0], fire[1])):
             x, y = col_to_x(c-1) + 0.25, row_to_y(image.shape[0], r-1) + 0.25
+            x += location[0] - cx
+            y += location[1] - cy
 
             weight = 0
             for (dr, dc) in neighbors:
                 if expand_image[r + dr, c + dc] == 0:
                     weight += 1
                 elif expand_image[r + dr, c + dc] == -1:
-                    weight += 0.15
+                    weight += 0.2
 
-            if weight > 0:
-                tasks.append([np.array([x, y]), weight])
+            task = np.around(np.array([x, y]), decimals=2)
+            if weight > 0 and not any(np.array_equal(task, m) for m in memory):
+                tasks.append([task, weight])
 
     tasks_ordered = []
     for i in range(len(tasks)):
         if i == 0:
-            p = np.array([cx, cy])
+            # p = np.array([cx, cy])
+            p = location
         else:
             p = tasks_ordered[-1][0]
 
-        tasks.sort(key=lambda s: s[1]/np.maximum(np.linalg.norm(s[0]-p, ord=2), 1), reverse=True)
+        # tasks.sort(key=lambda s: (np.linalg.norm(s[0]-p, ord=2), -s[1]))
+        tasks.sort(key=lambda s: (s[1]/len(neighbors))-np.maximum(np.linalg.norm(s[0]-p, ord=2), 1), reverse=True)
         tasks_ordered.append(tasks[0])
-        del tasks[0]
+        tasks = tasks[1:]
 
-    tasks_ordered = [[t[0]] for t in tasks_ordered]
+    # print(tasks_ordered)
+
+    tasks_ordered = [t[0] for t in tasks_ordered]
     return tasks_ordered
+
+
+def CreatePlan(tasks, initial_position):
+    d = 0
+    x0 = initial_position
+
+    actions = []
+    path = []
+    for idx, task in enumerate(tasks):
+        if idx > 0:
+            x0 = next_x0
+
+        T = np.maximum(int(np.ceil((1/0.5)*np.linalg.norm(x0-task, ord=2))), 1)
+        x = cvxpy.Variable((2, T+1))
+        u = cvxpy.Variable((2, T))
+
+        states = []
+        for t in range(T):
+            cost = cvxpy.maximum(cvxpy.norm(x[:, t+1]-task, p=2)-d, 0)
+            constraints = [x[:, t+1] == x[:, t] + u[:, t], cvxpy.norm(u[:, t], p=2) <= 0.5]
+            states.append(cvxpy.Problem(cvxpy.Minimize(cost), constraints))
+
+        cost = 0
+        constraints = [x[:, 0] == x0, cvxpy.maximum(cvxpy.norm(x[:, T]-task, p=2)-d, 0) <= 0]
+        states.append(cvxpy.Problem(cvxpy.Minimize(cost), constraints))
+
+        problem = cvxpy.sum(states)
+        problem.solve()
+
+        next_x0 = x.value[:, T]
+        path.append(x.value)
+        actions.append(u.value)
+
+    return actions, path
 
 
 if __name__ == "__main__":
@@ -132,8 +199,8 @@ if __name__ == "__main__":
     np.random.seed(42)
 
     agents = {1: {}}
-    agents[1]['position'] = np.array([9.75, 10.75])
-    # agents[1]['position'] = np.array([-0.25, -0.25])
+    agents[1]['position'] = np.array([9.75, 9.75])
+    agents[1]['memory'] = []
 
     grid_size = 50
     sim = FireSimulator(grid_size)
@@ -142,122 +209,40 @@ if __name__ == "__main__":
         sim.step([])
 
     # plot forest and agent position
-    ax = PlotForest(sim.state, grid_size)
-    ax.plot(agents[1]['position'][0], agents[1]['position'][1], Marker='.', MarkerSize=10, color='blue')
+    # ax = PlotForest(sim.state)
+    # ax.plot(agents[1]['position'][0], agents[1]['position'][1], Marker='.', MarkerSize=10, color='blue')
 
-    # get agent image and plot
-    r = int(-2*agents[1]['position'][1] + grid_size - 1 + 0.5)
-    c = int(2*agents[1]['position'][0] - 0.5)
+    for iteration in range(30):
+        print('iteration: %d' %iteration)
+        # if iteration > 20:
+        #     ax = PlotForest(sim.state)
 
-    image = CreateImage(sim.state, (r,c), (5,5))
-    ax = PlotForest(image, 5)
-    # ax.plot(1.25, 1.25, Marker='.', MarkerSize=20, color='blue')
+        # get agent image and plot
+        # r = int(-2*(agents[1]['position'][1]-0.25) + grid_size - 1)
+        # c = int(2*(agents[1]['position'][0]-0.25))
+        r, c = xy_to_rc(grid_size, agents[1]['position']-0.25)
+        image = CreateImage(sim.state, (r, c), (5, 5))
+        ax = PlotForestImage(image, agents[1]['position'])
+        ax.plot(agents[1]['position'][0], agents[1]['position'][1], Marker='.', MarkerSize=10, color='blue')
 
-    # test_image = copy.copy(image).astype('int32')
-    # test_image[test_image == 2] = 1
-    # dx = ndimage.sobel(test_image, 0)
-    # dy = ndimage.sobel(test_image, 1)
-    # magnitude = np.hypot(dx, dy)
-    # magnitude *= 255.0 / np.max(magnitude)
-    #
-    # magnitude[magnitude < 50] = 0
-    # magnitude[magnitude >= 50] = 1
-    # ax = PlotForest(magnitude, 5)
+        # get tasks from image, accounting for memory
+        tasks = CreateTasks(image, agents[1]['position'], agents[1]['memory'])
 
-    # r_fire, c_fire = np.where(image == 1)
-    # r_burn, c_burn = np.where(image == 2)
-    # targets = []
-    # means = np.zeros((len(r_fire)+len(r_burn), 2))
-    # covs = np.zeros((2, 2, len(r_fire)+len(r_burn)))
-    #
-    # for idx, (r, c) in enumerate(zip(r_fire, c_fire)):
-    #     x = c * 0.5 + 0.25
-    #     y = 5/2 - 0.5 * (r + 1) + 0.25
-    #
-    #     targets.append(np.array([x, y]))
-    #
-    #     means[idx, :] = [x, y]
-    #     covs[:, :, idx] = 0.25*np.eye(2, 2)
-    #
-    # for idx, (r, c) in enumerate(zip(r_burn, c_burn)):
-    #     x = c * 0.5 + 0.25
-    #     y = 5/2 - 0.5 * (r + 1) + 0.25
-    #
-    #     means[len(r_fire)+idx, :] = [x, y]
-    #     covs[:, :, len(r_fire)+idx] = 0.1*np.eye(2, 2)
+        # solve convex program to generate path
+        actions, path = CreatePlan(tasks, agents[1]['position'])
+        agents[1]['position'] = path[0][:, -1]
 
-    # create and plot heat map
-    # x = np.linspace(0, 4, num=100, endpoint=True)
-    # y = np.linspace(0, 4, num=100, endpoint=True)
-    # X, Y = np.meshgrid(x, y)
-    # Z = np.zeros_like(X)
-    # pos = np.dstack((X, Y))
+        # if iteration > 20:
+        ax.plot(path[0][0, :], path[0][1, :], Marker='.', MarkerSize=10, color='white')
 
-    # Z = np.zeros_like(X)
-    # for r in range(X.shape[0]):
-    #     for c in range(X.shape[1]):
-    #
-    #         point = np.array([X[r, c], Y[r, c]])
-    #         for idx in range(len(r_fire)):
-    #
-    #             vector = point - means[idx, :]
-    #             result = -0.5*np.matmul(np.matmul(vector.transpose(), np.linalg.inv(covs[:, :, idx])), vector)
-    #             result += np.log(1/np.sqrt((2*np.pi)**2*np.linalg.det(covs[:, :, i])))
-    #             Z[r, c] += np.exp(result)
+        # add task to memory
+        agents[1]['memory'].append(np.around(path[0][:, -1], decimals=2))
 
-    # for i in range(len(r_fire)):
-    #     Z += multivariate_normal.pdf(pos, mean=means[i, :], cov=covs[:, :, i])
-    # Z = multivariate_normal.pdf(pos, mean=means, cov=covs)
-    # Z /= np.linalg.norm(Z)
+        # retain tasks still in view
+        agents[1]['memory'] = [m for m in agents[1]['memory']
+                               if -5/4 <= m[0]-agents[1]['position'][0] <= 5/4 and
+                                  -5/4 <= m[1]-agents[1]['position'][1] <= 5/4]
 
-    # fig = pyplot.figure()
-    # ax = fig.add_subplot(121, aspect='equal')
-    # ax.pcolor(X, Y, Z)
-    # ax = fig.add_subplot(122, aspect='equal', projection='3d')
-    # ax.scatter(X, Y, Z)
+        # print(agents[1]['memory'])
 
-    # get tasks from image
-    tasks = CreateTasks(image)
-    tasks = [t[0] for t in tasks]
-    print(tasks)
-
-    # solve convex program to generate path
-    d = 0
-    x_0 = np.array([1.25, 1.25])
-    for idx, task in enumerate(tasks):
-        if idx > 0:
-            x_0 = next_x_0
-
-        T = np.maximum(int(np.ceil((1/0.5)*np.linalg.norm(x_0 - task, ord=2))), 1)
-        x = cvxpy.Variable((2, T+1))
-        u = cvxpy.Variable((2, T))
-
-        states = []
-        for t in range(T):
-            cost = cvxpy.maximum(cvxpy.norm(x[:, t+1]-task, p=2) - d, 0)
-            constraints = [x[:, t+1] == x[:, t] + u[:, t],
-                           cvxpy.norm(u[:, t], p=2) <= 0.5]
-
-            states.append(cvxpy.Problem(cvxpy.Minimize(cost), constraints))
-
-        constraints = [x[:, 0] == x_0, cvxpy.maximum(cvxpy.norm(x[:, T] - task, p=2) - d, 0) <= 0]
-        states.append(cvxpy.Problem(cvxpy.Minimize(0), constraints))
-
-        problem = cvxpy.sum(states)
-        problem.solve()
-
-        # print(problem.status)
-        # print(T)
-        # print(np.linalg.norm(u.value, axis=0))
-        # print()
-
-        next_x_0 = x.value[:, T]
-
-        ax.plot(x.value[0, :], x.value[1, :], Marker='.', Markersize=10, color='white', zorder=1)
-
-        # if np.linalg.norm(w.value[:, T-1]) > 0:
-        #     ax.plot(x.value[0, T-1:], x.value[1, T-1:], Marker='.', Markersize=10, color='blue', zorder=2)
-        #    # ax.plot(x.value[0, T], x.value[1, T], Marker='.', Markersize=10, color='blue', zorder=2)
-
-    pyplot.show()
-
+        pyplot.show()
