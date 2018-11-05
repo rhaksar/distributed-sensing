d4114d3f5310e171c951a9c88f8318d9573a6d2c
Ravi Haksar <rhaksar@stanford.edu>
2018-11-05 15:28:03 +0100

    Updated simulator to allow for non-square forests, varying persistence and propagation parameters, and added error checking for invalid probabilities. Converted simulator to perform all operations on (row, column) positions instead of (x, y).

--- FireSimulator.py
+++ FireSimulator.py
@@ -1,202 +1,221 @@
 import itertools
+from collections import defaultdict
 import math
 import numpy as np
+import warnings
+
 
 class FireSimulator(object):
-  """
-  A simulation of a forest fire using a discrete probabilistic grid model
-  The model is adapted from literature:
-    A. Somanath, S. Karaman, and K. Youcef-Toumi, "Controlling stochastic
-    growth processes on lattices: Wildfire management with robotic fire
-    extinguishers," in 53rd IEEE Conference on Decision and Control.
-    IEEE, 2014, pp. 1432-1327
-
-  """
-  def __init__(self, grid_size, rng=None, fire_init=None, 
-               alpha=0.2763, beta=np.exp(-1/10)):
     """
-    Initializes a simulation object.
-    Each tree in the grid model has three states: 
-      0 - healthy tree
-      1 - on fire tree
-      2 - burnt tree
-
-    Inputs:
-    - grid_size: size of forest, assumes square grid (minimum size: 4)
-    - rng: random number generator seed to use for deterministic sampling
-    - fire_init: list of tuples of (x,y) coordinates describing 
-                 positions of initial fires
-    - alpha: fire propagation parameter
-    - beta: fire persistence parameter
+    A simulation of a forest fire using a discrete probabilistic grid model
+    The model is adapted from literature:
+      A. Somanath, S. Karaman, and K. Youcef-Toumi, "Controlling stochastic
+      growth processes on lattices: Wildfire management with robotic fire
+      extinguishers," in 53rd IEEE Conference on Decision and Control.
+      IEEE, 2014, pp. 1432-1327
     """
 
-    self.grid_size = grid_size
-    self.alpha = alpha
-    self.beta = beta
+    def __init__(self, dimension, rng=None, fire_init=None,
+                 alpha=None, beta=None, model='exponential'):
+        """
+        Initializes a simulation object.
+        Each tree in the grid model has three states:
+          0 - healthy tree
+          1 - on fire tree
+          2 - burnt tree
 
-    self.state = np.zeros((grid_size,grid_size)).astype(np.uint8)
+        Inputs:
+        - dimension: size of forest, integer or tuple of integers
+                     if an integer, the forest is a square grid
+        - rng: random number generator seed to use for deterministic sampling
+        - fire_init: list of tuples of (r, c) coordinates describing
+                     positions of initial fires
+        - alpha: fire propagation parameter, as a defaultdict with array indices (r, c) as keys
+        - beta: fire persistence parameter, as a defaultdict with array indices (r, c) as keys
+        - model: method to convert fire propagation parameter to probability of catching on fire
+                 options - 'linear' or 'exponential'
+        """
 
-    self.stats = np.zeros(3).astype(np.uint32)
-    self.stats[0] += grid_size**2
+        self.dims = (dimension, dimension) if isinstance(dimension, int) else dimension
 
-    self.hlth_state = 0
-    self.burn_state = 1
-    self.dead_state = 2
+        self.alpha = defaultdict(lambda: 1-0.2763) if alpha is None else alpha
+        self.beta = defaultdict(lambda: np.exp(-1/10)) if beta is None else beta
+        self.model = model
 
-    if rng is not None:
-      np.random.seed(rng)
+        self.state = np.zeros(self.dims).astype(np.uint8)
 
-    self.iter = 0
-    self.fires = []
-    if fire_init is not None:
-      self.fires = fire_init
-      self.iter += 1
-      for (x,y) in fire_init:
-        c = x_to_col(x)
-        r = y_to_row(grid_size,y)
-        self.state[r,c] = 1
+        # number of [healthy, on fire, burnt] trees
+        self.stats = np.zeros(3).astype(np.uint32)
+        self.stats[0] += self.dims[0]*self.dims[1]
 
-      self.stats[0] -= len(fire_init)
-      self.stats[1] += len(fire_init)
-    
-    self.end = False
+        # tree states and neighbor set
+        self.healthy = 0
+        self.on_fire = 1
+        self.burnt = 2
+        self.neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-  def _persists(self, fire, action, dbeta):
-    """
-    Helper function to determine if a fire should continue to burn. Also
-    defines how the action affects the state update.
+        if rng is not None:
+            np.random.seed(rng)
 
-    Inputs:
-    - fire: tuple of (x,y) position 
-    - action: list of trees that should be treated at current time step
-    - dbeta: reduction in fire persistence parameter if tree is treated
+        # initialize fires at initial time
+        self.iter = 0
+        self.fires = []
+        if fire_init is not None:
+            self.fires = fire_init
+            self.iter += 1
+            for (r, c) in fire_init:
+                self.state[r, c] = 1
 
-    Returns:
-      True if fire continues to burn
-      False if fire burns out
-    """
+            self.stats[0] -= len(fire_init)
+            self.stats[1] += len(fire_init)
+        else:
+            self._start_fire()
 
-    delta = dbeta if fire in action else 0
-    sample = np.random.rand()
-    if sample > (self.beta - delta):
-      x,y = fire
-      c = x_to_col(x)
-      r = y_to_row(self.grid_size,y)
-      self.state[r,c] = 2
-      self.stats[1] -= 1
-      self.stats[2] += 1
-      return False
-    return True
+        self.end = False
+        self.early_end = False
+        return
 
-  def step(self, action, dbeta=0):
-    """
-    Function to update the state of the forest fire
+    def _start_fire(self):
+        """
+        Helper function to specify initial fire locations in the forest.
+        Assumes forest is at least 4x4 size.
 
-    Inputs:
-    - action: list of tuples of (x,y) coordinates describing treatment location
-    - dbeta: reduction in fire persistent parameter due to treatment
-    """
+        Inputs/Outputs:
+         None
+        """
+        if self.dims[0] < 4 or self.dims[1] < 4:
+            raise ValueError('Fire initialization requires a forest size of at least 4x4')
 
-    if self.end:
-      print("process has terminated")
-      return
+        # start a square of fires at center
+        r_center, c_center = math.ceil(self.dims[0]/2), math.ceil(self.dims[1]/2)
+        delta = [k for k in range(-1, 3)]
+        delta = itertools.product(delta, delta)
 
-    grid_size = self.grid_size
+        for (dr, dc) in delta:
+            r, c = r_center+dr, c_center+dc
+            self.fires.append((r, c))
+            self.state[r, c] = 1
 
-    # start a square of fires at center at iter 0
-    if self.iter == 0:
-      x = math.ceil(grid_size/2)
-      deltas = [k for k in range(-1,3)]
-      #deltas = [0]
-      
-      neighbors = itertools.product(deltas,deltas)
+        self.stats[0] -= len(self.fires)
+        self.stats[1] += len(self.fires)
+        self.iter += 1
+        return
 
-      for (dx,dy) in neighbors:
-        xn = x + dx
-        yn = x + dy
+    def _persists(self, fire, action, dbeta):
+        """
+        Helper function to determine if a fire should continue to burn. Also
+        defines how the action affects the state update.
 
-        cn = x_to_col(xn)
-        rn = y_to_row(grid_size,yn)
+        Inputs:
+        - fire: tuple of (r, c) position
+        - action: list of trees that should be treated at current time step
+        - dbeta: reduction in fire persistence parameter if tree is treated
 
-        self.fires.append((xn,yn))
-        self.state[rn,cn] = 1
-        self.stats[0] -= 1
-        self.stats[1] += 1
+        Returns:
+          True if fire continues to burn
+          False if fire burns out
+        """
 
-      self.iter += 1
-      return
+        delta = dbeta if fire in action else 0
+        threshold = self.beta[fire]-delta
 
-    neighbors = [(-1,0),(1,0),(0,-1),(0,1)]
+        # check for invalid probability
+        if threshold > 1:
+            warnings.warn('Fire persistence parameter and control effect are set such that '
+                          'the probability of persisting is greater than one')
+        elif threshold < 0:
+            warnings.warn('Fire persistence parameter and control effect are set such that '
+                          'the probability of persisting is less than zero')
 
-    add = [] # list of trees that will catch on fire
-    checked = [] # list of healthy trees that have been simulated
-    num_fires = len(self.fires)
+        sample = np.random.rand()
+        if sample > threshold:
+            self.state[fire[0], fire[1]] = 2
+            self.stats[1] -= 1
+            self.stats[2] += 1
+            return False
+        return True
 
-    # fire spread step
-    # iterate over fires, find their healthy neighbors, and sample to 
-    # determine if they catch fire
-    for (x,y) in self.fires:
+    def step(self, action, dbeta=0):
+        """
+        Method to update the state of the forest fire.
 
-      for (dx,dy) in neighbors:
-        xn = x + dx
-        yn = y + dy
+        Inputs:
+        - action: list of tuples of (r, c) indices describing treatment location
+        - dbeta: reduction in fire persistent parameter due to treatment
+        """
 
-        if xn>=1 and xn<=grid_size and yn>=1 and yn<=grid_size:
-          cn = x_to_col(xn)
-          rn = y_to_row(grid_size,yn)
+        if self.end:
+            print("fire extinguished")
+            return
 
-          if self.state[rn,cn] == self.hlth_state and (xn,yn) not in checked:
-            num_neighbor_fires = 0
+        # assume that fires are not able to spread to healthy trees
+        # flag will be False if there exists a fire with at least one healthy neighbor
+        self.early_end = True
 
-            for (dx2,dy2) in neighbors:
-              xn2 = xn + dx2
-              yn2 = yn + dy2
+        add = []  # list of trees that will catch on fire
+        checked = []  # list of healthy trees that have been simulated
 
-              if xn2>=1 and xn2<=grid_size and yn2>=1 and yn2<=grid_size:
-                cn2 = x_to_col(xn2)
-                rn2 = y_to_row(grid_size,yn2)
+        # fire spread step:
+        #     iterate over fires, find their healthy neighbors, and sample to
+        #     determine if each healthy neighbor catches fire
+        for (r, c) in self.fires:
 
-                if self.state[rn2,cn2] == self.burn_state:
-                  num_neighbor_fires += 1
+            for (dr, dc) in self.neighbors:
+                rn, cn = r + dr, c + dc
 
-            prob = 1 - (1 - self.alpha)**num_neighbor_fires # exponential model
-            #prob = self.alpha*num_neighbor_fires # linear model
-            if np.random.rand() <= prob:
-              add.append((xn,yn))
+                # check for neighbors of fires that are healthy
+                # and have not been simulated yet
+                if 0 <= rn < self.dims[0] and 0 <= cn < self.dims[1]:
+                    if (rn, cn) not in checked and self.state[rn, cn] == self.healthy:
+                        self.early_end = False
 
-            checked.append((xn,yn))
+                        # determine the number of trees on fire surrounding the healthy tree
+                        num_neighbor_fires = 0
+                        for (dr2, dc2) in self.neighbors:
+                            rn2, cn2 = rn + dr2, cn + dc2
 
-    # fire burn out step [includes action]
-    self.fires = [(x,y) for (x,y) in self.fires if self._persists((x,y),action,dbeta)]
+                            if 0 <= rn2 < self.dims[0] and 0 <= cn2 < self.dims[1]:
+                                if self.state[rn2, cn2] == self.on_fire:
+                                    num_neighbor_fires += 1
 
-    # update list of current fires
-    for (x,y) in add:
-      c = x_to_col(x)
-      r = y_to_row(grid_size,y)
-      self.fires.append((x,y))
-      self.state[r,c] = 1
+                        # determine the probability of catching on fire and sample
+                        if self.model == 'exponential':
+                            p = 1-(self.alpha[(r, c)]**num_neighbor_fires)
+                        elif self.model == 'linear':
+                            p = self.alpha[(r, c)]*num_neighbor_fires
+                        else:
+                            raise ValueError('Invalid model name provided')
 
-    self.stats[0] -= len(add)
-    self.stats[1] += len(add)
+                        # check for invalid probability
+                        if p < 0:
+                            warnings.warn('Fire propagation parameter is set such that the '
+                                          'probability of catching on fire is less than zero')
+                        if p > 1:
+                            warnings.warn('Fire propagation parameter is set such that the '
+                                          'probability of catching on fire is greater than one')
 
-    # terminate if no fires
-    if not self.fires:
-      self.iter += 1
-      self.end = True
-      return
+                        # sample to determine if healthy tree catches on fire
+                        if np.random.rand() <= p:
+                            add.append((rn, cn))
 
-    self.iter += 1
-    return
+                        checked.append((rn, cn))
 
-# helper functions to map between (row,col) and (x,y)
-def col_to_x(col):
-  return col+1
+        # fire burn out step, including action
+        self.fires = [fire for fire in self.fires if self._persists(fire, action, dbeta)]
 
-def x_to_col(x):
-  return x-1
+        # update list of current fires
+        for (r, c) in add:
+            self.fires.append((r, c))
+            self.state[r, c] = 1
 
-def row_to_y(grid_size,row):
-  return grid_size-row
+        self.stats[0] -= len(add)
+        self.stats[1] += len(add)
 
-def y_to_row(grid_size,y):
-  return grid_size-y
+        # terminate if no fires
+        if not self.fires:
+            self.iter += 1
+            self.end = True
+            return
+
+        self.iter += 1
+        return
