24fd3047d5abc3840c5ca7dc85a7152e69e2868a
Ravi Haksar <rhaksar@stanford.edu>
2020-12-23 14:30:04 -0800

    Cleaned up file and simplified class to prevent errors in printing.

--- framework/uav.py
+++ framework/uav.py
@@ -1,22 +1,23 @@
-import numpy as np
-
-
 class UAV(object):
+    """
+    Aerial vehicle definition. 
+    """
 
-    def __init__(self, label=None, position=None, belief=None, image_size=None):
+    def __init__(self, label, belief, image_size):
+        # a numeric id
         self.label = label
-        # self.deployed = False
 
-        self.position = position
+        self.position = ()
 
         self.image_size = image_size
         self.belief = belief
 
-        self.budget = None
-        self.first = None
-        self.last = None
+        # planning budget and meeting locations
+        self.budget = 0
+        self.first = ()
+        self.last = ()
 
-        # self.meetings = []
+        # sequence of positions indicating planned trajectory, and plans by other UAVs, to prevent duplicate coverage
         self.plan = []
         self.other_plans = dict()
 
@@ -29,14 +30,3 @@ class UAV(object):
         return 'UAV ' + str(self.label) + '\n' + 'position: ' + str(self.position) + '\n' + 'first: ' + \
                str(self.first) + '\n' + 'last: ' + str(self.last) + '\n' + 'budget: ' + str(self.budget) + \
                '\n' + 'plan: ' + str(self.plan) + '\n' + 'other plans: ' + str(self.other_plans)
-
-    # def deploy(self, time, config):
-    #     if self.deployed:
-    #         return
-    #
-    #     # check if correct time to be deployed
-    #     if (time-1)%config.deploy_interval==0 and self.id <= 2*((time-1)/config.deploy_interval + 1):
-    #         self.position = config.deploy_locations[1] if self.id%2==0 else config.deploy_locations[0]
-    #         self.deployed = True
-    #
-    #     return
